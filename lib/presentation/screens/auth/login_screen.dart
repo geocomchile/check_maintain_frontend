8021e76ae6a27037bf2731c134c9c3b48d944818
obedmacallums <obedmacallums@gmail.com>
2023-12-15 09:14:36 -0300

    Refactor login screen and add custom form

--- lib/presentation/screens/auth/login_screen.dart
+++ lib/presentation/screens/auth/login_screen.dart
@@ -10,9 +10,59 @@ class LoginScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Login'),
       ),
-      body: const Center(
-        child: Text('Login '),
-      ),
+      body: const MyCustomForm()
+    );
+  }
+}
+
+class MyCustomForm extends StatelessWidget {
+  const MyCustomForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(15))),
+              hintText: 'username',
+              labelText: 'Username',
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: TextFormField(
+            obscureText: true,
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(15))),
+              labelText: 'Password',
+              hintText: 'Enter your password',
+            ),
+          ),
+        ),
+                Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: Row(children: [
+            Expanded(
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(colors.secondaryContainer),
+                ),
+                onPressed: () {},
+                child: Text('Login',  style: TextStyle(color: colors.onBackground)),
+              ),
+            ),
+            const SizedBox(width: 10,),
+
+
+          ],)
+        ),
+      ],
     );
   }
 }
