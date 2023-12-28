5f1cfbf01e3fc202c34243b2fe574e8e1d1149e5
obedmacallums <obedmacallums@gmail.com>
2023-12-28 08:21:28 -0300

    Add DLRegisterController and Get package imports to DeviceScreen

--- lib/presentation/screens/device/device_screen.dart
+++ lib/presentation/screens/device/device_screen.dart
@@ -1,5 +1,7 @@
+import 'package:check_maintain_frontend/presentation/controllers/dl_register_controller.dart';
 import 'package:check_maintain_frontend/presentation/widgets/charts/collimation_error_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class DeviceScreen extends StatefulWidget {
   static const String name = 'device-screen';
@@ -12,11 +14,21 @@ class DeviceScreen extends StatefulWidget {
 }
 
 class _DeviceScreenState extends State<DeviceScreen> {
+  final dlRegisterController = Get.find<DLRegisterController>();
+  @override
+  void initState() {
+    super.initState();
+    dlRegisterController
+        .getRegistersByDeviceId(widget.idDevice)
+        .then((value) => print(value.first.collimationError))
+        .onError((error, stackTrace) => printError(info: error.toString()));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Device'),
+        title: Text('Device ${widget.idDevice}'),
       ),
       body: const Padding(
         padding: EdgeInsets.all(16.0),
