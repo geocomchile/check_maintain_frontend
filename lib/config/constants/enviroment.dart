667de9f8be8a2652dfc6d3c2b7a425dd0e0f719f
obedmacallums <obedmacallums@gmail.com>
2024-01-22 15:39:47 -0300

    Update environment configuration

--- lib/config/constants/enviroment.dart
+++ lib/config/constants/enviroment.dart
@@ -2,7 +2,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Environment {
   static initEnvironment() async {
-    await dotenv.load(fileName: '.env');
+    await dotenv.load(fileName: 'env');
   }
 
   static String apiUrl =
